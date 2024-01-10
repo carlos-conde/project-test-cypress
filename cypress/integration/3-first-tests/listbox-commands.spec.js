9060a0d71ce0c1ec9a78e31638f1be20c87a68ee
carlos-conde <carlos.conde.santos@gmail.com>
2024-01-10 12:30:10 +0100

    first tests//added a custom command

--- cypress/integration/3-first-tests/listbox-commands.spec.js
+++ cypress/integration/3-first-tests/listbox-commands.spec.js
@@ -3,13 +3,15 @@ describe("listbox commands", () => {
         cy.visit("/");
     });
 
-    it("enter to the home page", () => {
+    it("check title content", () => {
+        cy.customLog("check title content");
         cy.fixture("index").then((index) => {
             cy.get(index.title).should("contain", index.titleResult);
         });
     });
 
-    it("to click on commands listbox ", () => {
+    it("to click on commands listbox", () => {
+        cy.customLog("to click on commands listbox");
         cy.fixture("index").then((index) => {
             cy.get(index.listboxCommands).click();
             cy.get(index.listboxCommandsResult).should("be.visible");

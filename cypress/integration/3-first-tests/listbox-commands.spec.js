618945bdd6f98745398157da8d167f1945f9ad51
carlos-conde <carlos.conde.santos@gmail.com>
2024-01-10 13:03:41 +0100

    first tests//added commands-first-tests

--- cypress/integration/3-first-tests/listbox-commands.spec.js
+++ cypress/integration/3-first-tests/listbox-commands.spec.js
@@ -5,6 +5,7 @@ describe("listbox commands", () => {
 
     it("check title content", () => {
         cy.customLog("check title content");
+        cy.customLogFT("check title content");
         cy.fixture("index").then((index) => {
             cy.get(index.title).should("contain", index.titleResult);
         });
@@ -12,6 +13,7 @@ describe("listbox commands", () => {
 
     it("to click on commands listbox", () => {
         cy.customLog("to click on commands listbox");
+        cy.customLogFT("to click on commands listbox");
         cy.fixture("index").then((index) => {
             cy.get(index.listboxCommands).click();
             cy.get(index.listboxCommandsResult).should("be.visible");
